5b2395a8d0d1b0b3595901bfdafb4240917caa38
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-05-27 15:58:26 +0800

    Menambahkan Objek ibu dan anak

--- Agregasi/agregasi.cpp
+++ Agregasi/agregasi.cpp
@@ -4,3 +4,11 @@ using namespace std;
 
 #include "ibu.h"
 #include "anak.h"
+
+int main() {
+    ibu* varIbu = new ibu("dini");
+    ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("dewi");
+}
